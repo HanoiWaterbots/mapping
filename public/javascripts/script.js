3304cc2e2aac13fa2a7e5c6a0e8bb0512eb2e903
utkarsh867 <utkarsh867@gmail.com>
2018-06-03 16:30:37 +0530

    establish socket connection

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -9,6 +9,6 @@ serverRequest("/getLocation", (res) =>{
     locationMarker.setLatLng(L.latLng(coords.lat, coords.lng));
 });
 
-socketConnection('localhost:8080', (data)=>{
+socketConnection('http://ec2-18-205-17-12.compute-1.amazonaws.com:8080', (data)=>{
     console.log(data);
 });
